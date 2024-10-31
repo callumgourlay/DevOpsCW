e2dc62ed2fb0d450914fa1f525c04c4a5acc5c69
Callum Gourlay <65352339+callumgourlay@users.noreply.github.com>
2024-10-31 15:33:51 +0000

    change from junit jupiter as used before to junit

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -1,5 +1,5 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
